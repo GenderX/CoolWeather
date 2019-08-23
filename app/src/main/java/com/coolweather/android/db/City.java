3b93082549de7b24c2be36f0e399f6b1be4b62d7
lican <lican@qq.com>
2019-08-23 17:34:49 +0800

    Initial commit

---
+++ app/src/main/java/com/coolweather/android/db/City.java
@@ -0,0 +1,11 @@
+package com.coolweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+public class City extends DataSupport {
+    private int id;
+    private String cityName;
+    private int cityCode;
+    private int provinceId;
+    
+}
