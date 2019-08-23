493c9f587e3b6aea253bde4852d404e237f57d8a
lican <lican@qq.com>
2019-08-23 17:42:00 +0800

    数据库配置

---
+++ app/src/main/java/com/coolweather/android/db/County.java
@@ -0,0 +1,42 @@
+package com.coolweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+public class County extends DataSupport {
+    private  int id;
+    private String countryName;
+    private String weatherId;
+    private int getId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getGetId() {
+        return getId;
+    }
+
+    public void setGetId(int getId) {
+        this.getId = getId;
+    }
+}
